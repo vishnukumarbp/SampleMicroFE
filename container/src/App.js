997409391878f5f4d14ed42d66ae65c838103e65
vishnukumar <vishnulalmaddy@gmail.com>
2021-07-14 11:57:56 +0530

    Initial Commit

---
+++ container/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => (
+  <div>
+    <h1>Hi from Container</h1>
+    <hr />
+    <MarketingApp />
+  </div>
+);
