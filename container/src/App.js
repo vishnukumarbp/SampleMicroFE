7686daf7a920814cac908906598cc9dd017d3f37
vishnukumar <vishnulalmaddy@gmail.com>
2021-07-15 11:30:53 +0530

    Update yml file and app.js

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => (
   <div>
-    <h1>Hi from Container</h1>
-    <hr />
-    <MarketingApp />
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
   </div>
 );
