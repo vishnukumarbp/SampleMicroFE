2be3426780c4c70ba52083236ba0038d3de954db
vishnukumar <vishnulalmaddy@gmail.com>
2021-07-15 08:40:59 +0530

    Trigger deployment

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => (
   <div>
-    <h1>Hi from Container</h1>
+    <h1>Hi from Container!!</h1>
     <hr />
     <MarketingApp />
   </div>
